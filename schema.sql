53103794a033787996b8c7bbc3ee7f1c27d26959
Aneal Laryea <aneallaryea100@gmail.com>
2022-09-20 22:38:00 +0000

    add queries

--- schema.sql
+++ schema.sql
@@ -1,5 +1,9 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    name varchar(100)
-);
+    id INT NOT NULL GENERATED ALWAYS AS IDENTITY,
+    name varchar(200) NOT NULL,
+    date_of_birth DATE NOT NULL,
+    escape_attempts INT NOT NULL,
+    neutured BOOLEAN NOT NULL,
+    weight_kg DECIMAL NOT NULL );
