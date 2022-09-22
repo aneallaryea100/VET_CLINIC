9f28924118ec126e66e3e1c804c64887e479756b
Aneal Laryea <aneallaryea100@gmail.com>
2022-09-22 12:19:23 +0000

    multiple tables

--- data.sql
+++ data.sql
@@ -13,3 +13,47 @@ INSERT INTO animals (name, date_of_birth, escape_attempts, neutured, weight_kg) 
 ('Boarmon', date '2005-06-07', 7, true, 20.4),
 ('Blossom', date '1998-10-13', 3, true, 17),
 ('Ditto', date '2022-05-14', 4, true, 22);
+
+
+-- Insert the following data into the owners table:
+-- Sam Smith 34 years old.
+-- Jennifer Orwell 19 years old.
+-- Bob 45 years old.
+-- Melody Pond 77 years old.
+-- Dean Winchester 14 years old.
+-- Jodie Whittaker 38 years old.
+
+INSERT INTO owners (full_name, age) VALUES ('Sam Smith', 34),
+('Jennifer Orwell', 19),
+('Bob', 45),
+('Melody Pond', 77),
+('Dean Winchester', 14),
+('Jodie Whittaker', 38);
+
+
+-- Insert the following data into the species table:
+-- Pokemon
+-- Digimon
+
+INSERT INTO species (name) VALUES ('Pokemon'), ('Digimon');
+
+-- Modify your inserted animals so it includes the species_id value:
+-- If the name ends in "mon" it will be Digimon
+-- All other animals are Pokemon
+
+UPDATE animals SET species_id = 2 WHERE name LIKE '%mon';
+UPDATE animals SET species_id = 1 WHERE species_id IS NULL;
+
+
+-- Modify your inserted animals to include owner information (owner_id):
+-- Sam Smith owns Agumon.
+-- Jennifer Orwell owns Gabumon and Pikachu.
+-- Bob owns Devimon and Plantmon.
+-- Melody Pond owns Charmander, Squirtle, and Blossom.
+-- Dean Winchester owns Angemon and Boarmon.
+
+UPDATE animals SET owner_id = 1 WHERE name = 'Agumon';
+UPDATE animals SET owner_id = 2 WHERE name = 'Gabumon' OR name = 'Pikachu';
+UPDATE animals SET owner_id = 3 WHERE name = 'Devimon' OR name = 'Plantmon';
+UPDATE animals SET owner_id = 4 WHERE name = 'Charmander' OR name = 'Squirtle' OR name = 'Blossom';
+UPDATE animals SET owner_id = 5 WHERE name = 'Angemon' OR name = 'Boarmon';
--- schema.sql
+++ schema.sql
@@ -10,3 +10,42 @@ CREATE TABLE animals (
 
     ALTER TABLE animals
     ADD species VARCHAR(200);
+
+
+
+-- Create a table named owners with the following columns:
+-- id: integer (set it as autoincremented PRIMARY KEY)
+-- full_name: string
+-- age: integer
+
+CREATE TABLE owners (
+    id INT NOT NULL GENERATED ALWAYS AS IDENTITY,
+    full_name varchar(200) NOT NULL,
+    age INT NOT NULL,
+    PRIMARY KEY (id) );
+
+--     Create a table named species with the following columns:
+-- id: integer (set it as autoincremented PRIMARY KEY)
+-- name: string
+
+CREATE TABLE species (
+    id INT NOT NULL GENERATED ALWAYS AS IDENTITY,
+    name varchar(200) NOT NULL,
+    PRIMARY KEY (id) );
+
+-- Modify animals table:
+-- Make sure that id is set as autoincremented PRIMARY KEY
+-- Remove column species
+-- Add column species_id which is a foreign key referencing species table
+-- Add column owner_id which is a foreign key referencing the owners table
+
+     ALTER TABLE animals
+    ADD PRIMARY KEY (id),
+    DROP species,
+    ADD COLUMN species_id INT,
+    ADD CONSTRAINT fk_animals_species, 
+    FOREIGN KEY (species_id) REFERENCES species (id);
+
+    ALTER TABLE animals
+    ADD COLUMN owner_id INT
+    ADD CONSTRAINT fk_animals_owners FOREIGN KEY (owner_id) REFERENCES owners (id);
